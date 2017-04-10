ee5c362ff4503f613eb225c68a1733365902c08e
pidan1231239 <850576570@qq.com>
2017-04-10 17:35:39 +0800

    测试了freeRTOS在被编码器中断打断下的运行情况

--- ebox_stm32f103RCT6_VS/ebox_stm32f103RCT6_VS/user/main.cpp
+++ ebox_stm32f103RCT6_VS/ebox_stm32f103RCT6_VS/user/main.cpp
@@ -13,28 +13,47 @@
 
 #include "ebox.h"
 #include "encoder_exti.h"
+#include "freertos.h"
+#include "task.h"
+#include "queue.h"
+#include "led.h"
 
+EncoderExti encoder1(&PA6, &PA7);
+Led led1(&PA8,1);
 
-
-EncoderExti encoder1(&PA8, &PA7);
+long long oldPos = 0, nowPos = 0;
+static void vLEDTask(void *pvParameters)
+{
+	while (1)
+	{
+		led1.toggle();
+		vTaskDelay(100 / portTICK_RATE_MS);
+		nowPos = encoder1.getPosition();
+		uart1.printf("%ld\r\n", nowPos/*- oldPos*/);
+		oldPos = nowPos;
+	}
+}
 
 void setup()
 {
     ebox_init();
     uart1.begin(115200);
+	led1.begin();
+
+	set_systick_user_event_per_sec(configTICK_RATE_HZ);
+	attach_systick_user_event(xPortSysTickHandler);
+
+	xTaskCreate(vLEDTask, "LED0", configMINIMAL_STACK_SIZE, (void *)0, NULL, NULL);
+	vTaskStartScheduler();
 }
 
 int main(void)
 {
     setup();
-	long long oldPos = 0, nowPos = 0;
 
     while(1)
     {
-		nowPos = encoder1.getPosition();
-        uart1.printf("%ld\r\n", nowPos/*- oldPos*/);
-		oldPos = nowPos;
-        delay_ms(100);
+
     }
 }
 
